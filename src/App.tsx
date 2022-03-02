437140b5a3db1e9eb1152eddf5f3d02e995ac086
Rael Calitro <rael06@hotmail.fr>
2022-03-02 12:32:06 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.scss';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.App}>
+      <header className={classes['App-header']}>
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={() => alert("Clicked")}
+        >Material UI Button</Button>
+        <img src={logo} className={classes['App-logo']} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={classes['App-link']}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
