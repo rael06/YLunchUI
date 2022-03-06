94d01fde7f7b40a510ecc096d55043ea9e0c6f6e
Rael Calitro <rael06@hotmail.fr>
2022-03-06 18:22:56 +0100

    add request

--- src/LoginPage/index.tsx
+++ src/LoginPage/index.tsx
@@ -3,12 +3,20 @@ import React from "react";
 import classes from "./styles.module.scss";
 
 export default function LoginPage() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = React.useState("admin@restaurant.com");
+  const [password, setPassword] = React.useState("Password1234.");
 
   function handleClick() {
     console.log(email);
     console.log(password);
+    const body = { email, password };
+    fetch("https://ylunch-api.rael-calitro.ovh/authentication/login", {
+      method: "post",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(body),
+    });
   }
 
   return (
