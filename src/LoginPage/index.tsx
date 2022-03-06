b069e3332ea414ef30a00184b8b791457cdab465
Rael Calitro <rael06@hotmail.fr>
2022-03-06 18:03:56 +0100

    add-form

--- src/LoginPage/index.tsx
+++ src/LoginPage/index.tsx
@@ -1,14 +1,33 @@
-import { Button } from "@material-ui/core";
-import { TextField } from "material-ui";
+import { Button, TextField } from "@material-ui/core";
 import React from "react";
 import classes from "./styles.module.scss";
 
 export default function LoginPage() {
   const [email, setEmail] = React.useState("");
-  function handleClick() {}
+  const [password, setPassword] = React.useState("");
+
+  function handleClick() {
+    console.log(email);
+    console.log(password);
+  }
+
   return (
     <div className={classes.wrapper}>
-      <TextField name="email">Email</TextField>
+      <TextField
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        label="Email"
+      />
+
+      <TextField
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        label="Password"
+        type="password"
+      />
+
       <Button onClick={handleClick}>Confirmer</Button>
     </div>
   );
