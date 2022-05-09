d1e90d2a5c88314b4e9ba02fc5c705b956ec4db6
Rael Calitro <rael06@hotmail.fr>
2022-05-09 22:59:05 +0200

    improve cart message when empty

--- src/CustomerApp/components/Cart/index.tsx
+++ src/CustomerApp/components/Cart/index.tsx
@@ -90,154 +90,158 @@ export default function Cart() {
       <Typography mb={2} variant="h2" component="h1">
         Mon panier
       </Typography>
-      <Card>
-        <Container maxWidth="md">
-          <CardContent>
-            {cart.items.length > 0 ? (
-              <>
-                <TableContainer>
-                  <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                      <TableRow>
-                        <TableCell sx={{ fontWeight: "bold" }}>Nom</TableCell>
-                        <TableCell sx={{ fontWeight: "bold" }} align="center">
-                          Quantité
-                        </TableCell>
-                        <TableCell sx={{ fontWeight: "bold" }} align="center">
-                          Prix total
-                        </TableCell>
-                        <TableCell sx={{ fontWeight: "bold" }} align="center">
-                          Retirer
-                        </TableCell>
-                        <TableCell sx={{ fontWeight: "bold" }} align="center">
-                          Ajouter
-                        </TableCell>
+      {cart.items.length > 0 ? (
+        <Card>
+          <Container maxWidth="md">
+            <CardContent>
+              <TableContainer>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell sx={{ fontWeight: "bold" }}>Nom</TableCell>
+                      <TableCell sx={{ fontWeight: "bold" }} align="center">
+                        Quantité
+                      </TableCell>
+                      <TableCell sx={{ fontWeight: "bold" }} align="center">
+                        Prix total
+                      </TableCell>
+                      <TableCell sx={{ fontWeight: "bold" }} align="center">
+                        Retirer
+                      </TableCell>
+                      <TableCell sx={{ fontWeight: "bold" }} align="center">
+                        Ajouter
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {cart.items.map((cartItem) => (
+                      <TableRow
+                        key={cartItem.product.id}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <CartItem
+                          product={cartItem.product}
+                          quantity={cartItem.quantity}
+                          addProduct={addProduct}
+                          removeProduct={removeProduct}
+                        />
                       </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {cart.items.map((cartItem) => (
-                        <TableRow
-                          key={cartItem.product.id}
-                          sx={{
-                            "&:last-child td, &:last-child th": { border: 0 },
-                          }}
-                        >
-                          <CartItem
-                            product={cartItem.product}
-                            quantity={cartItem.quantity}
-                            addProduct={addProduct}
-                            removeProduct={removeProduct}
-                          />
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-                <Box sx={{ display: "flex", flexDirection: "column" }}>
-                  {!!totalPrice && (
-                    <Typography m={2}>
-                      Prix total : {`${totalPrice.toFixed(2)}`.padStart(2, "0")}{" "}
-                      €
-                    </Typography>
-                  )}
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      paddingY: 2,
-                    }}
-                    component={"form"}
-                    onSubmit={handleSubmit((formData) =>
-                      confirmOrder(formData)
-                    )}
-                  >
-                    {" "}
-                    <FormInput
-                      register={register}
-                      label="Commentaire"
-                      name="customerComment"
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                {!!totalPrice && (
+                  <Typography m={2}>
+                    Prix total : {`${totalPrice.toFixed(2)}`.padStart(2, "0")} €
+                  </Typography>
+                )}
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingY: 2,
+                  }}
+                  component={"form"}
+                  onSubmit={handleSubmit((formData) => confirmOrder(formData))}
+                >
+                  {" "}
+                  <FormInput
+                    register={register}
+                    label="Commentaire"
+                    name="customerComment"
+                  />
+                  <Box sx={{ display: "flex" }}>
+                    <ProgressButton
+                      label="Confirmer la réservation"
+                      sx={{ marginTop: "10px" }}
+                      disabled={isCartEmpty || isNotLoggedInWhenConfirmOrder}
+                      status={status}
+                      type="submit"
                     />
-                    <Box sx={{ display: "flex" }}>
-                      <ProgressButton
-                        label="Confirmer la réservation"
-                        sx={{ marginTop: "10px" }}
-                        disabled={isCartEmpty || isNotLoggedInWhenConfirmOrder}
-                        status={status}
-                        type="submit"
-                      />
-                      <Button
-                        sx={{ marginTop: "10px" }}
-                        variant="outlined"
-                        onClick={clearCart}
-                        disabled={isCartEmpty}
-                      >
-                        Supprimer le panier
-                      </Button>
-                    </Box>
+                    <Button
+                      sx={{ marginTop: "10px" }}
+                      variant="outlined"
+                      onClick={clearCart}
+                      disabled={isCartEmpty}
+                    >
+                      Supprimer le panier
+                    </Button>
                   </Box>
-                  {isNotLoggedInWhenConfirmOrder && (
-                    <Typography color="error">
-                      Veuillez vous{" "}
-                      <span
-                        onClick={() => navigate("/customer/login")}
-                        style={{ cursor: "pointer", color: "#1976d2" }}
-                      >
-                        connecter
-                      </span>{" "}
-                      pour enregistrer votre réservation.
-                    </Typography>
-                  )}
-                  {currentUser && isOrderSucceed && (
-                    <Typography>
-                      Votre réservation a bien été enregistrée, elle est en
-                      attente d'acceptation par le restaurant. Vous pouvez
-                      consulter son avancée dans la section{" "}
-                      <span
-                        onClick={() => navigate("/customer/orders")}
-                        style={{ cursor: "pointer", color: "#1976d2" }}
-                      >
-                        Mes réservations
-                      </span>
-                      .
-                    </Typography>
-                  )}
-                  <Typography
-                    color="error"
-                    visibility={
-                      addOrderApiError &&
-                      addOrderApiError.status === 400 &&
-                      addOrderApiError.errors.reasons.includes(
-                        "ReservedForDateTime must be set when the restaurant is open for orders."
-                      )
-                        ? "visible"
-                        : "hidden"
-                    }
-                  >
-                    L'horaire de retrait doit être compris dans les horaires
-                    d'ouverture à la commande du restaurant
-                  </Typography>
-                  <Typography
-                    color="error"
-                    visibility={
-                      addOrderApiError &&
-                      addOrderApiError.status === 400 &&
-                      !addOrderApiError.errors.reasons.includes(
-                        "ReservedForDateTime must be set when the restaurant is open for orders."
-                      )
-                        ? "visible"
-                        : "hidden"
-                    }
-                  >
-                    {translateApiErrors(addOrderApiError, "Réservation")}
-                  </Typography>
                 </Box>
-              </>
-            ) : (
-              <Typography>Panier vide</Typography>
-            )}
-          </CardContent>
-        </Container>
-      </Card>
+                {isNotLoggedInWhenConfirmOrder && (
+                  <Typography color="error">
+                    Veuillez vous{" "}
+                    <span
+                      onClick={() => navigate("/customer/login")}
+                      style={{ cursor: "pointer", color: "#1976d2" }}
+                    >
+                      connecter
+                    </span>{" "}
+                    pour enregistrer votre réservation.
+                  </Typography>
+                )}
+                {currentUser && isOrderSucceed && (
+                  <Typography>
+                    Votre réservation a bien été enregistrée, elle est en
+                    attente d'acceptation par le restaurant. Vous pouvez
+                    consulter son avancée dans la section{" "}
+                    <span
+                      onClick={() => navigate("/customer/orders")}
+                      style={{ cursor: "pointer", color: "#1976d2" }}
+                    >
+                      Mes réservations
+                    </span>
+                    .
+                  </Typography>
+                )}
+                <Typography
+                  color="error"
+                  visibility={
+                    addOrderApiError &&
+                    addOrderApiError.status === 400 &&
+                    addOrderApiError.errors.reasons.includes(
+                      "ReservedForDateTime must be set when the restaurant is open for orders."
+                    )
+                      ? "visible"
+                      : "hidden"
+                  }
+                >
+                  L'horaire de retrait doit être compris dans les horaires
+                  d'ouverture à la commande du restaurant
+                </Typography>
+                <Typography
+                  color="error"
+                  visibility={
+                    addOrderApiError &&
+                    addOrderApiError.status === 400 &&
+                    !addOrderApiError.errors.reasons.includes(
+                      "ReservedForDateTime must be set when the restaurant is open for orders."
+                    )
+                      ? "visible"
+                      : "hidden"
+                  }
+                >
+                  {translateApiErrors(addOrderApiError, "Réservation")}
+                </Typography>
+              </Box>
+            </CardContent>
+          </Container>
+        </Card>
+      ) : (
+        <Typography>
+          Votre panier est vide. Choisissez un des{" "}
+          <span
+            onClick={() => navigate("/customer/restaurants")}
+            style={{ cursor: "pointer", color: "#1976d2" }}
+          >
+            restaurants
+          </span>{" "}
+          et réservez-y des produits.
+        </Typography>
+      )}
     </Container>
   );
 }
